802baea69601f5f7c005ac44d82562dfcdca681b
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-21 17:03:13 -0600

    Add answers to ifStatements.js

--- ifStatements.js
+++ ifStatements.js
@@ -4,14 +4,14 @@
 // (while navigated to the codeChallenges directory):
 // `node ifStatements.js`
 
-// Example: Using the weather variable below, write code that decides 
+// Example: Using the weather variable below, write code that decides
 // what you should take with you based on the following conditions:
   // if it is sunny, print "sunscreen"
   // if it is rainy, print "umbrella"
   // if it is snowy, print "coat"
   // if it is icy, print "yak traks"
 
-weather = "sunny"
+var weather = "rainy"
 
 if (weather === "sunny") {
   console.log("sunscreen");
@@ -29,28 +29,35 @@ if (weather === "sunny") {
 // to print something other than 'coat'
 
 
-##################
+//##################
 // Using the numQuarters variable defined below, determine
 // if you have enough money to buy a gumball. A gumball costs
 // two quarters.
 
 // Right now, the program will print
 // out both "I have enough money for a gumball" and
-// "I don't have enough money for a gumball". Write a 
+// "I don't have enough money for a gumball". Write a
 // conditional statement that prints only one or the other.
 
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
 numQuarters = 0
+// Ternary operator heck yeah!
+numQuarters >= 2 ? console.log("I have enough money for a gumball") : console.log("I don't have enough money for a gumball");
 
-console.log("I have enough money for a gumball");
-console.log("I don't have enough money for a gumball");
+// if else
+if ( numQuarters >=2 ){
+  console.log("I have enough money for a gumball")
+} else {
+  console.log("I don't have enough money for a gumball");
+
+}
 
 
 // #####################
 // Using the variables defined below, write code that will tell you
-// if you have the ingredients to make a pizza. A pizza requires 
+// if you have the ingredients to make a pizza. A pizza requires
 // at least two cups of flour and sauce.
 
 // You should be able to change the variables to achieve the following outconsole.log();
@@ -62,6 +69,13 @@ console.log("I don't have enough money for a gumball");
 // Experiment with manipulating the value held within both variables
 // to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 2;
 var hasSauce = true;
 
+var enoughFlour = cupsOfFlour >= 2
+
+if ( enoughFlour && hasSauce ) {
+  console.log("I can make pizza")
+} else {
+  console.log("I cannot make pizza")
+}
