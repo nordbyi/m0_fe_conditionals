b4523a8af69fc2edacb7e70380c394fc65dadc7d
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-21 15:33:32 -0600

    Initial commit

--- comparisons.js
+++ comparisons.js
@@ -21,24 +21,31 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+console.log(numberTeachers < numberStudents)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+console.log(numberTeachers === stringTeachers)
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log(numberTeachers !== numberStudents)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log(numberStudents >= 20)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log(numberStudents >= 21)
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log(numberStudents <= 20)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log(numberStudents <= 21)
 // this should log: true
 
 
@@ -53,21 +60,26 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 
 console.log(4 < 9);
 // YOU DO: Explain.
+// This will log the result of whether 4 is less than 9.  It will evaulate to true
 
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
+// This will log the result of whether 4 is less than the value stored in books (which is 3). It will evaluate to false
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
 // YOU DO: Explain.
+// This will log the result of whether the value stored in friends (6) is greater than the value stored in siblings (2).
+// It will evaluate to true
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees != meals);
 // YOU DO: Explain.
-
+// This will log the result of whether the value stored in attendees (9) is NOT equal to the value stored in meals (8).
+// This will evaluate to true
 
 // #-------------------
 // PART 3: Logical Operators
@@ -89,15 +101,17 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
-
+console.log(lovesToPlay && lovesTreats)
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark)
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark)
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age < 1)
 
-// What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// What did your final line of code evaluate to? Why do you think that is? Explain
+// ANSWER: You know, it's hard to find a definitive source on what age a dog is considered a puppy. 
+// So I went with the more conservative 1 year.  Though, based on the question above, I feel as though I've missed something...
